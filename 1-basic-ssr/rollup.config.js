940c47035100b7eefedc85bee1eceebfe409cfdc
Nathan Kessler <nkessler@base2.io>
2020-12-08 12:40:17 -0500

    Fix client-side rendering & cleanup

--- 1-basic-ssr/rollup.config.js
+++ 1-basic-ssr/rollup.config.js
@@ -3,7 +3,6 @@ import dev from 'rollup-plugin-dev'
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 
 export default {
